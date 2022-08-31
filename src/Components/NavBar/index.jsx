0d7c002e76732cb4cfcf2781e2578a7c2aeaf92b
Fer Taype <lfertm08@gmail.com>
2022-08-30 23:09:09 -0300

    Fresh start

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './styles.scss'
+const NavBar = () => {
+    return (
+    <header className='head'>
+            <div>
+                <section className='mainlogo'>Gaia
+                </section>
+            </div>
+                <section className='mobilenav'>
+                    <nav className='mobvar'>
+                    <ul>
+                        <li><a href="index.html"><i className='fa-solid fa-house'></i></a></li>
+                        <li><a href="contacto.html"><i className='fa-solid fa-address-card'></i></a></li>
+                        <li><a href="productos.html"><i className='fa-solid fa-basket-shopping'></i></a></li>
+                        <li><a href="tienda.html"> <i className='fa-solid fa-cart-shopping'></i></a></li>
+                    </ul> 
+                    <input type="text" placeholder="Buscar"/>
+                    </nav>
+                </section>
+    </header>
+    
+)
+}
+
+export default NavBar;
