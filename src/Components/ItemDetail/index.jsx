4a5aa28b6def473f262f04e261ca3808da6f0057
Fer Taype <lfertm08@gmail.com>
2022-09-14 22:36:05 -0300

    Detalle de producto 1.1

--- src/Components/ItemDetail/index.jsx
+++ src/Components/ItemDetail/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ItemCount from '../ItemCount'
-const ItemDetail = ({product}) => {
+const ItemDetail = ({products}) => {
 return (
     <div>
         <div className='card col-lg-6 col-xl-6 col-md-8 col-sm-8 goods'>
-                    <img className='card-img-top' src= {product.img} alt= {product.name} />
+                    <img className='card-img-top' src= {products.img} alt= {products.name} />
                     <div className='card-body'>
-                        <h4 className='card-title'>{product.name}</h4>
-                        <p className='card-text'>{product.description}. Precio unitario: $<span class="pesos">{product.price}</span></p>
-                        <strong>Stock: {product.stock}</strong>
-                        <ItemCount stock={product.stock} initial={1} onAdd/>
+                        <h4 className='card-title'>{products.name}</h4>
+                        <p className='card-text'>{products.description}. Precio unitario: $<span class="pesos">{products.price}</span></p>
+                        <strong>Stock: {products.stock}</strong>
+                        <ItemCount stock={products.stock} initial={1} onAdd/>
                         <button className='btn btn-primary comprar nro'>Añadir al carro</button>
                     </div>
             </div>
--- src/data/products.js
+++ src/data/products.js
@@ -2,7 +2,7 @@ export const products = [
 {id:1, description:"Stenotaphrum secundatum, Recipiente N°14,", name: "Césped" ,stock: 4, price: 460, img: '"../Item/CardImg/grass-g51bcb9d5d_640.jpg"'},
 {id:2, description:"Bonsai N°3,", name: "Bonsai moyogi" ,stock: 2, price: 3150, img: './Item/CardImg/bonsai-gc7173a5d7_640.jpg'},
 {id:3, description:"Set de 5 herramientas,", name: "Kit de jardineria Terra" ,stock: 5, price: 1300, img: '../Item/CardImg/tools.jpg'},
-{id:4, description:"Recipente ceramico N°5,", name: "Maceta mediana negra" ,stock: 10, price: 680, img: '../Item/CardImg/blackpot.jpg'},
+{id:4, description:"Recipente ceramico N°5,", name: "Maceta mediana negra" ,stock: 10, price: 680, img: './Item/CardImg/blackpot.jpg'},
 {id:5, description:"Castaño (árbol) maceta N°4,", name: "Castanea sativa" ,stock: 8, price: 1050, img: '../Item/CardImg/castaño.jpg'},
 {id:6, description:"Recipiente de ceramica con diseño N°3,", name: "Maceta zorro" ,stock: 8, price: 580, img: './Item/funnypot.jpg'}
 ]
